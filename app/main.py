2185df0fabec7ad58ad688126de47dcc586068b2
sangaje <qhals8380@gmail.com>
2025-07-18 16:46:32 +0900

    Make entry point

---
+++ app/main.py
@@ -0,0 +1,15 @@
+"""
+Entry point for the FastAPI application.
+This file initializes the FastAPI app and includes the API router.
+"""
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from app.web.routers import router as web_router
+
+# Initialize the FastAPI application
+app = FastAPI(title="Drone Delivery Server", version="0.0.1", debug=True)
+
+app.mount("/static", StaticFiles(directory="app/web/static"), name="static")
+app.include_router(web_router, prefix="")
