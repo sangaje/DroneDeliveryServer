916e79c762564b6c559ccfa92392d203caa2b5fc
sangaje <qhals8380@gmail.com>
2025-07-29 14:30:15 +0900

    Add additional dependency for MyPy type checking and reorder import statements in airsim_config.py

--- app/web/routers/airsim_config.py
+++ app/web/routers/airsim_config.py
@@ -3,9 +3,9 @@
 from typing import Any
 
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from fastapi.responses import HTMLResponse
 router = APIRouter(prefix="/airsim_config")
 templates = Jinja2Templates(directory="app/web/templates")
 
