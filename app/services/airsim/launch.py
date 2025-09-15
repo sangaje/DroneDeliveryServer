b90c8aec1dcb1ed427800c62cbb7a795efb17bae
sangaje <qhals8380@gmail.com>
2025-09-15 14:44:17 +0900

    Refactor simulation session class and improve drone creation logic

--- app/services/airsim/launch.py
+++ app/services/airsim/launch.py
@@ -1,14 +1,20 @@
 """TODO: Launch AirSim simulation with specified configurations."""
 
 from app.configs import Config
+from app.models.drone import DroneStatus
 from app.services.controller.database import drop_all_tables, init_db
 from app.services.controller.drone_service import create_drone
-from app.utils.airsimutils import Drone, connect_client, create_drones_list, disconnect_client
+from app.utils.airsimutils import (
+    Drone,
+    connect_client,
+    create_drones_list,
+    disconnect_client,
+)
 
 from .config import AirSimConfig
 
 
-class SimulationSession(Config):
+class _SimulationSession(Config):
     """Class to manage a simulation session with AirSim."""
 
     _airsim_config: AirSimConfig
@@ -39,10 +45,21 @@ class SimulationSession(Config):
 
         # Create drones in the database
         for drone in self._drones:
-            # TODO: change to use drone.id when constructor is fixed
-            _ = drone
-            create_drone()
+            create_drone(
+                drone_id=drone.id,
+                status=DroneStatus.IDLE,
+                max_battery=10000,
+                cur_battery=10000,
+                max_payload=5,
+                cur_payload=0,
+                cur_lat=drone.state.gps_location.latitude,
+                cur_lon=drone.state.gps_location.longitude,
+                cur_alt=drone.state.gps_location.altitude,
+            )
 
     def end(self) -> None:
         """End the simulation session."""
         disconnect_client()
+
+
+simulation_session: _SimulationSession | None = None
